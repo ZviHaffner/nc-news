06f4860f11beed8e0d6bccd442743b916719c2ef
ZviHaffner <zvihaffner@Zvi-Haffners-Laptop>
2024-06-18 08:14:43 +0100

    Creates article page and functionality to select an article on the home page and view it

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,6 +1,6 @@
+import { Link } from 'react-router-dom';
+
 export const ArticleCard = ({ article }) => {
-  const author =
-    article.author.charAt(0).toUpperCase() + article.author.slice(1);
   const topic = article.topic.charAt(0).toUpperCase() + article.topic.slice(1);
   const date = article.created_at.split("T")[0];
 
@@ -8,9 +8,10 @@ export const ArticleCard = ({ article }) => {
     <section className="article-card">
       <h2>{article.title}</h2>
       <img src={article.article_img_url} id="article-img" />
-      <p>By: {author}</p>
+      <p>By: {article.author}</p>
       <p>Topic: {topic}</p>
       <p>Date Created: {date}</p>
+      <Link to={`/articles/${article.article_id}`}>Read Article</Link>
     </section>
   );
 };
