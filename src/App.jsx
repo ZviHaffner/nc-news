132fd32556315980001375825f13e74e36c2aaf1
ZviHaffner <zvihaffner@Zvi-Haffners-Laptop>
2024-06-20 16:08:23 +0100

    Handles non-existent path error route

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import { Header } from "./components/Header";
 import { Nav } from "./components/Nav";
 import { Article } from "./components/Article";
 import { Users } from "./components/Users";
+import { ErrorPage } from "./components/ErrorPage";
 
 function App() {
-
   return (
     <>
       <Header />
       <Nav />
       <Routes>
+        <Route path="*" element={<ErrorPage />} />
         <Route path="/" element={<Home />} />
         <Route path="/articles/:article_id" element={<Article />} />
         <Route path="/topics/:topic" element={<Home />} />
---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,8 @@
+export const ErrorPage = () => {
+  return (
+    <>
+      <h2>404</h2>
+      <h3>Page Not Found</h3>
+    </>
+  );
+};
